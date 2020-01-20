761d57a74d3fb284155e13f9657b4c392b3e234a
Akiomi Kamakura <akiomik@gmail.com>
2020-01-20 11:21:34 +0900

    Add cli interface

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # precountify
 A tool for pre-countifying
+
+## Development
+
+```
+# install
+pipenv install
+
+# update `precountify` command
+pipenv install --upgrade .
+```
---
+++ precountify/cli.py
@@ -0,0 +1,14 @@
+import fire
+
+
+def run(bpm=None, meter=4, measure=2, upbeat=0):
+    # TODO
+    print(bpm, meter, measure, upbeat)
+
+
+def main():
+    fire.Fire(run)
+
+
+if __name__ == '__main__':
+    main()
